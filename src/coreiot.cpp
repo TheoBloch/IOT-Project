4da8d76ef6d192da031064bbb7b4333c148f3247
Jadele22 <jade.bloch@proton.me>
2025-12-05 17:54:29 +0700

    change to permit the build

--- src/coreiot.cpp
+++ src/coreiot.cpp
@@ -118,13 +118,13 @@ void coreiot_task(void *pvParameters){
         client.loop();
 
         // Sample payload, publish to 'v1/devices/me/telemetry'
-        String payload = "{\"temperature\":" + String(glob_temperature) +  ",\"humidity\":" + String(glob_humidity) + "}";
+        //String payload = "{\"temperature\":" + String(glob_temperature) +  ",\"humidity\":" + String(glob_humidity) + "}";
         
-        client.publish("v1/devices/me/telemetry", payload.c_str());
+       // client.publish("v1/devices/me/telemetry", payload.c_str());
 
 
         
-        Serial.println("Published payload: " + payload);
+        //Serial.println("Published payload: " + payload);
         vTaskDelay(10000);  // Publish every 10 seconds
     }
 }
